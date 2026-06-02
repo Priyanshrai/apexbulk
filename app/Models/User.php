7430c5e837845e9216e7ba807ab006f4b99b1689
Priyanshrai <182571043+developerprr@users.noreply.github.com>
2026-06-02 19:45:56 +0530

    feat: add Shopify app configuration and job handling
    - Updated the home route to include the 'verify.shopify' middleware.
    - Created a new job class 'AppUninstalledJob' extending the existing Shopify job.
    - Added a comprehensive configuration file for the Shopify app, including options for debugging, API credentials, billing, webhooks, and theme support.

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,13 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Osiset\ShopifyApp\Contracts\ShopModel as IShopModel;
+use Osiset\ShopifyApp\Traits\ShopModel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements IShopModel
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, ShopModel;
 
     /**
      * The attributes that are mass assignable.
@@ -43,7 +45,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+           // 'password' => 'hashed',
         ];
     }
 }
